5f0e5e4110bb12e348a5765cd3cc1bd974b8d5eb
Unknown <seredominguez@hotmail.com>
2018-07-11 17:59:34 -0300

    A

--- SerenaDominguezE4/SerenaDominguezE4/ViewController.swift
+++ SerenaDominguezE4/SerenaDominguezE4/ViewController.swift
@@ -11,18 +11,65 @@ import FirebaseUI
 import Kingfisher
 
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FUIAuthDelegate {
+    
+    var authUI: FUIAuth?
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.authUI = FUIAuth.defaultAuthUI()
+        self.authUI?.delegate = self
+        
+        
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth()
+        ]
+        self.authUI?.providers = providers
+        
+        if let currentUser = Auth.auth().currentUser {
+            if Auth.auth().currentUser != nil {
+                do {
+                    self.performSegue(withIdentifier: "primerSegue", sender: self)
+                    }
+                }
+        } else {
+            loginButton.setTitle("LOGIN", for: .normal)
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
- 
-
+    @IBAction func loginTappedButton(_ sender: Any) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+            } catch {}
+            
+            loginButton.setTitle("LOGIN", for: .normal)
+//            label.text = ""
+            
+        } else {
+            if let authViewController = self.authUI?.authViewController() {
+                present(authViewController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+//        label.text = user?.displayName
+//        imageView.kf.setImage(with: user?.photoURL)
+        if let currentUser = Auth.auth().currentUser {
+            if Auth.auth().currentUser != nil {
+                do {
+        self.performSegue(withIdentifier: "primerSegue", sender: self)
+        loginButton.setTitle("LOGOUT", for: .normal)
+                }
+    }
+    }
+}
 }
 
---
+++ SerenaDominguezE4/SerenaDominguezE4/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  SerenaDominguezE4
+//
+//  Created by Digital House on 11/7/18.
+//  Copyright © 2018 Digital House. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+    }
+
+}
